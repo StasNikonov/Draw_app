1805245510009a507e28e33ee8b41a27ba69acb9
StasNikonov <nikonoff.stanislav2005@gmail.com>
2024-06-08 14:24:23 +0300

    Initial commit. Created the main "painting canvas" screen and added a panel for choosing line colors.

---
+++ app/src/main/java/com/example/draw_app/PathData.kt
@@ -0,0 +1,10 @@
+package com.example.draw_app
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+
+data class PathData(
+    val path: Path = Path(),
+    val color: Color = Color.Black
+
+    )
